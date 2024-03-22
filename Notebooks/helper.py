48c3c113f94f6b5a3599185a75fe771dd4b381ca
egerc <eger.christian@gmx.de>
2024-03-22 12:44:46 +0100

    added dataprep fct and model eval fct

--- Notebooks/helper.py
+++ Notebooks/helper.py
@@ -2,6 +2,16 @@ import torch
 from torch.utils.data import TensorDataset
 import numpy as np
 
+def return_label_maps_from_adata(adata, category):
+    label_to_id = {label: idx for idx, label in enumerate(adata.obs[category].unique())}
+    id_to_label = {idx: label for label, idx in label_to_id.items()}
+    return(label_to_id, id_to_label)
+
+def return_dataset_sizes(adata, ratio, pct_of_data=1):
+    obs_num = round(adata.shape[0]*pct_of_data)
+    train_obs_num = round(obs_num*ratio)
+    test_obs_num = obs_num - train_obs_num
+    return(train_obs_num, test_obs_num)
 
 # This function takes in your adata object as well as the category you want to train your data on as well as parameters for defining the datasets sizes and returns two pytorch datasets, one for training and one for testing
 def obs_to_tensor(adata, category=None, training_size=None, test_size=None):
@@ -24,8 +34,7 @@ def obs_to_tensor(adata, category=None, training_size=None, test_size=None):
         return TensorDataset(tensors, labels)
 
     # The purpose of these dictionaries is to encode the values of the desired category as integers
-    label_to_id = {label: idx for idx, label in enumerate(adata.obs[category].unique())}
-    id_to_label = {idx: label for label, idx in label_to_id.items()}
+    label_to_id, id_to_label = return_label_maps_from_adata(adata, category)
 
     # Making sure the total dataset size doesnt exceed the number of observations in the adata object
     if test_size + training_size <= adata.shape[0]:
@@ -48,3 +57,24 @@ def obs_to_tensor(adata, category=None, training_size=None, test_size=None):
         testing_data,
         id_to_label,
     )
+
+
+def evaluate_model(model, dataset, labels_map):
+
+    def outputs_to_labels(output, labels_map):
+        predicted_index = torch.argmax(output).item()
+        predicted_label = labels_map[predicted_index]
+        return predicted_label
+
+    correct_guesses = []
+    for data, label in dataset:
+        model_output = model(data)
+        predicted_label = outputs_to_labels(model_output, labels_map)
+        actual_label = labels_map[int(label)]
+        if predicted_label == actual_label:
+            correct_guesses.append(predicted_label)
+        else:
+            continue
+    
+    print(f'The models accuracy is {(len(correct_guesses)/len(dataset))*100}%')
+    print(f'The accuracy of random classification would be {(1/len(labels_map)*100)}%')
